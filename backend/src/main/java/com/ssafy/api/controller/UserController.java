8b03f0087b2a1d3e3cee96f3482987ac4e0f5488
정혜원 <dnjs3023@gmail.com>
2026-01-16 00:21:07 +0900

    docs: 누락된 API 문서 생성

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -94,7 +94,7 @@ public class UserController {
 	@ApiOperation(value = "회원 정보 수정", notes = "회원 정보를 수정한다.")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "성공"),
-			@ApiResponse(code = 403, message = "인증 실패"),
+			@ApiResponse(code = 403, message = "권한 없음"),
 			@ApiResponse(code = 500, message = "서버 오류")
 	})
 	public ResponseEntity<String> updateUserInfo(@PathVariable("userId") String userId
@@ -111,6 +111,12 @@ public class UserController {
 	}
 
 	@DeleteMapping("/{userId}")
+	@ApiOperation(value = "회원 탈퇴", notes = "회원 정보를 삭제한다.")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공"),
+			@ApiResponse(code = 403, message = "권한 없음"),
+			@ApiResponse(code = 500, message = "서버 오류")
+	})
 	public ResponseEntity<String> withdraw(@PathVariable("userId") String userId
 											, @ApiIgnore Authentication authentication) {
 		SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
