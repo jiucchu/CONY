68f876e7d5b79d88d07bc7e10bdfd61c834bbba8
unknown <jiucchu@gmail.com>
2026-01-16 14:09:02 +0900

    refactor: 사이즈 타입 추가

--- src/components/common/card/DdayView.tsx
+++ src/components/common/card/DdayView.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { COLORS } from "@/constants/colors";
+import { SizeType } from "@/types/common";
 
-const DdayContainer = styled.div`
+const DdayContainer = styled.div<{ size: number }>`
   background-color: #FFFFFF;
   border-radius: 100px;
   padding: 3px;
@@ -9,23 +10,24 @@ const DdayContainer = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
-  width: 65px;
+  width: ${props => props.size}px;
   background-color: ${COLORS.primary};
 `;
 
-const DdayText = styled.p`
+const DdayText = styled.p<{ size: number }>`
   font-family: 'pretendard', sans-serif;
-  font-size: 15px;
+  font-size: ${props => props.size}px;
   font-weight: 700;
   color: ${COLORS.text.white};
 `;
 
-const DdayComponent = ({ dday }: { dday: number }) => {
+const DdayView = ({ dday, size }: { dday: number, size: SizeType }) => {
+  console.log(size);
   return (
-    <DdayContainer>
-      <DdayText>D-{dday}</DdayText>
+    <DdayContainer size={size === 'Small' ? 65 : size === 'Medium' ? 75 : 90}>
+      <DdayText size={size === 'Small' ? 12 : size === 'Medium' ? 14 : 20}>D-{dday}</DdayText>
     </DdayContainer>
   );
 };
 
-export default DdayComponent;
+export default DdayView;
