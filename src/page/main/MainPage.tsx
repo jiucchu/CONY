367e217a16d8099054125a4eeac869648f5b51c1
unknown <jiucchu@gmail.com>
2026-01-16 17:22:32 +0900

    style: 메인페이지 배치

--- src/page/main/MainPage.tsx
+++ src/page/main/MainPage.tsx
@@ -1,17 +1,48 @@
+'use client';
+
 import styled from "styled-components";
 import { COLORS } from "@/constants/colors";
+import MainGiftCardCarousel from "@/components/common/card/MainGiftCardCarousel";
+import CouponList from "@/components/common/card/CouponList";
+import { getCoupons } from "@/mockDB/mock";
+import ContentLayout from "@/app/ContentLayout";
 
-const MainPageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: ${COLORS.background.white};
+const ContentArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
 `;
 
+
 const MainPage = () => {
+  const coupons = getCoupons();
+  
+  const handleMoreClick = () => {
+    console.log('더보기 클릭');
+  };
+
+
+  const content = (
+    <ContentArea>
+      <MainGiftCardCarousel coupons={coupons} />
+      <CouponList 
+        coupons={coupons} 
+        title="유효기간 임박 쿠폰"
+        onMoreClick={handleMoreClick}
+      />
+      
+      <CouponList 
+        coupons={coupons} 
+        title="유효기간 임박 쿠폰"
+        onMoreClick={handleMoreClick}
+      />
+    </ContentArea>
+  );
+
   return (
-    <MainPageContainer>
-      <h1>Main</h1>
-    </MainPageContainer>
+    <ContentLayout>
+      {content}
+    </ContentLayout>
   );
 };
 
