f81fb0217c5e97d5f9ece48b5b5450e900bd2ba2
unknown <jiucchu@gmail.com>
2026-01-16 12:54:46 +0900

    style: 디데이 크기 수정

--- src/components/common/card/DdayView.tsx
+++ src/components/common/card/DdayView.tsx
@@ -4,18 +4,18 @@ import { COLORS } from "@/constants/colors";
 const DdayContainer = styled.div`
   background-color: #FFFFFF;
   border-radius: 100px;
-  padding: 6px;
+  padding: 3px;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
-  width: 70px;
+  width: 65px;
   background-color: ${COLORS.primary};
 `;
 
 const DdayText = styled.p`
   font-family: 'pretendard', sans-serif;
-  font-size: 14px;
+  font-size: 15px;
   font-weight: 700;
   color: ${COLORS.text.white};
 `;
