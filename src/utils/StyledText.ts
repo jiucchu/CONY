7ffbdb152f3528da01939922ba1bd8241d9e2cac
unknown <jiucchu@gmail.com>
2026-01-20 08:43:35 +0900

    util: styled Text 수정

--- src/utils/StyledText.ts
+++ src/utils/StyledText.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const StyledText = styled.p<{ fontSize: number; fontWeight: number; color: string }>`
+const StyledText = styled.p<{ fontSize: number; fontWeight: number; color?: string }>`
   font-family: 'pretendard', sans-serif;
   font-size: ${props => props.fontSize}px;
   color: ${props => props.color};
