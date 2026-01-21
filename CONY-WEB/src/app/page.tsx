6d9904c76608508cb4a4666401563b29073a591f
GitHub Action <action@github.com>
2026-01-21 08:23:09 +0000

    Sync CONY-WEB from GitHub [Wed Jan 21 08:23:09 UTC 2026]

--- CONY-WEB/src/app/page.tsx
+++ CONY-WEB/src/app/page.tsx
@@ -1,12 +1,8 @@
-import MainGiftCard from "@/components/common/card/atomic/MainGiftCard";
-import { getCoupons } from "@/mockDB/mock";
-import LoginPage from "@/page/auth/LoginPage";
-import CouponList from "@/components/common/card/CouponList";
 import MainPage from "@/page/main/MainPage";
+
 export default function Home() {
   return (
     <>
-    {/* <CouponList coupons={getCoupons()} title="유효기간 임박 쿠폰"  /> */}
     <MainPage />
     </>
   );
