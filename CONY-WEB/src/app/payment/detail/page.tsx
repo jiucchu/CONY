2292f2fdfba909476b0178d304ab4150ee57abc3
박지유 <jiucchu@gmail.com>
2026-01-26 14:27:14 +0900

    [FE] 기프티콘 판매 디테일 페이지 구현 (!10)

---
+++ CONY-WEB/src/app/payment/detail/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import PaymentDetail from "@/page/payment/detail/Detail";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { getCoupons } from "@/mockDB/mock";
+import { useEffect, useState } from "react";
+import { Coupon } from "@/types/coupon/coupon";
+
+function PaymentDetailContent() {
+  const searchParams = useSearchParams();
+  const idParam = searchParams.get('id');
+  const couponId = idParam ? parseInt(idParam, 10) : 0;
+  const [coupon, setCoupon] = useState<Coupon | null>(null);
+
+  useEffect(() => {
+    const coupons = getCoupons();
+    const foundCoupon = coupons.find(c => c.coupon_id === couponId);
+    setCoupon(foundCoupon || null);
+  }, [couponId]);
+
+  if (!coupon) {
+    return (
+      <div>
+        <h1>쿠폰을 찾을 수 없습니다.</h1>
+      </div>
+    );
+  }
+
+  return <PaymentDetail coupon={coupon} />;
+}
+
+const PaymentDetailPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentDetailContent />
+    </Suspense>
+  );
+};
+
+export default PaymentDetailPage;
