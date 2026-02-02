d55b8dea8ae26dc0268be8f08e214328b3cef2ab
정혜원 <dnjs3023@gmail.com>
2026-02-02 23:51:06 +0900

    feat: 지오펜스 진입 이벤트 요청 dto (#73)

--- CONY-BE/manage/src/main/java/com/cony/manage/domain/geofence/dto/GeofenceDto.java
+++ CONY-BE/manage/src/main/java/com/cony/manage/domain/geofence/dto/GeofenceDto.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
