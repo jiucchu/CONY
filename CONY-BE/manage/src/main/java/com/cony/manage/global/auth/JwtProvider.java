b3d948948d17040d1985509229792bc3138b5a7c
정혜원 <dnjs3023@gmail.com>
2026-02-03 10:42:04 +0900

    chore: jwt 로그 추가

--- CONY-BE/manage/src/main/java/com/cony/manage/global/auth/JwtProvider.java
+++ CONY-BE/manage/src/main/java/com/cony/manage/global/auth/JwtProvider.java
@@ -79,13 +79,13 @@ public class JwtProvider {
                     .parseClaimsJws(token);
             return true;
         } catch (SecurityException | MalformedJwtException e) {
-            // log.info("잘못된 JWT 서명입니다.");
+            System.out.prinln("잘못된 JWT 서명입니다.\n" + e);
         } catch (ExpiredJwtException e) {
-            // log.info("만료된 JWT 토큰입니다.");
+            System.out.prinln("만료된 JWT 토큰입니다.\n" + e);
         } catch (UnsupportedJwtException e) {
-            // log.info("지원되지 않는 JWT 토큰입니다.");
+            System.out.prinln("지원되지 않는 JWT 토큰입니다.\n" + e);
         } catch (IllegalArgumentException e) {
-            // log.info("JWT 토큰이 잘못되었습니다.");
+            System.out.prinln("JWT 토큰이 잘못되었습니다.\n" + e);
         }
         return false;
     }
